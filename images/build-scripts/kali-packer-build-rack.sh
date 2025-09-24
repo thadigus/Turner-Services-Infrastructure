fe6bfd1bf581e26a2571250eaebec5ab70be2c6b
Thadigus <turner.w.thad@gmail.com>
2025-09-23 21:34:23 -0400

    Migrating to single step docker run for image builds to cut down on failures

--- images/build-scripts/kali-packer-build-rack.sh
+++ images/build-scripts/kali-packer-build-rack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export PKR_VAR_build_passwd_local=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 13; echo);
+git config --global --add safe.directory $PWD
 export git_curr_dir=$(git rev-parse --show-toplevel)
 /usr/bin/packer init -var "ansible_provisioner_playbook_path=deployment/linux-packer-config.yml" -var-file=$git_curr_dir/turner-services-sensitive-repo/provisioning-base-install-sensitive-rack.auto.pkrvars.hcl $git_curr_dir/images/kali-image-build-packer-on-proxmox/.
 /usr/bin/packer validate -var "ansible_provisioner_playbook_path=deployment/linux-packer-config.yml" -var-file=$git_curr_dir/turner-services-sensitive-repo/provisioning-base-install-sensitive-rack.auto.pkrvars.hcl $git_curr_dir/images/kali-image-build-packer-on-proxmox/.
