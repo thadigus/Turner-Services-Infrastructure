58a0f714ef1e84478625b4ac1e85d860c9d35d03
Thadigus <turner.w.thad@gmail.com>
2025-10-13 14:16:55 -0400

    Cleaning up inventories so that CI can see the Proxmox hosts

--- deployment/provision-vms.sh
+++ deployment/provision-vms.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 ansible-playbook --extra-vars @./turner-services-sensitive-repo/server-list-rack.yml --extra-vars @./turner-services-sensitive-repo/inventories/ansible-inv-rack.proxmox.yml ./deployment/create-virtual-machines.yml 
-#ansible-playbook --extra-vars @./turner-services-sensitive-repo/server-list-office.yml --extra-vars @./turner-services-sensitive-repo/inventories/ansible-inv-office.proxmox.yml ./deployment/create-virtual-machines.yml
